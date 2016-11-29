47302324128757333751aa3580c993f995c68681
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-11-29 19:40:43 +0200

    bind server with app

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
+var path    = require("path");
 
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
+
+app.use('/static', express.static(__dirname + '/static'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
